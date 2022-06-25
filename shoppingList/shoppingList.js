fe42e51582652b02faa1d914db4c51a8deaa264a
Byron <byron.southerland@icloud.com>
2022-06-25 14:25:40 -0400

    using objects

--- shoppingList/shoppingList.js
+++ shoppingList/shoppingList.js
@@ -1,50 +1,77 @@
-const itemTxt = document.getElementById('itemTxt');
-const addBtn = document.getElementById('addBtn');
-const removeFirstBtn = document.getElementById('removeFirstBtn');
-const removeFirstLast = document.getElementById('removeLastBtn');
-
-addBtn.addEventListener('click', readInput);
+const itemTxt = document.getElementById("itemTxt");
+const addBtn = document.getElementById("addBtn");
+const removeFirstBtn = document.getElementById("removeFirstBtn");
+const removeFirstLast = document.getElementById("removeLastBtn");
 
 
-let shoppingList = []
 
-function readInput() {
-    const input = itemTxt.value;
-    if (input) {
-        const items = input.split(',');
-        addItems(items);
-    } else {
-        showError('Please enter an item.');
-    }
-}
+addBtn.addEventListener("click", () => {
+	const input = itemTxt.value;
+	if (input) {
+		const items = input.split(",");
+		addItems(items);
+	} else {
+		showError("Please enter an item.");
+	}
+	dispaly();
+} );
+
+
+
+const syd = { userName: "Sydney", age: 27, address: 1231 };
+
+let users = [
+	{ userName: "MJ", age: 5, address: 1231 },
+	{ userName: "Byron", age: 50, address: 2912 }
+];
+
+
+let shoppingList = [];
+
+let myname = new String();
+let myage = number;
+let sydneyAge = string;
+
+myname = "Byron";
+myage = 50;
+
+sydneyAge = 27;
+let combinedAged = sydneyAge + myage;
+
+console.log();
+
+
 
 function addItems(items) {
-    shoppingList = shoppingList.concat(items);
-    console.log(shoppingList);
+	shoppingList = shoppingList.concat(items);
 }
 
+function removeFirst() {}
 
-function removeFirst() {
-
-}
-
-function removeLast() {
-
-}
+function removeLast() {}
 
 function dispaly() {
-
+	const itemList = document.getElementById("itemList");
+	for (i = 0; i < shoppingList.length; i++) {
+		itemList.appendChild(createListItem(shoppingList[i]));
+	}
 }
 
-function showError(errorMsg) {
-    const messages = document.getElementById('messages');
-    messages.style.background = 'red';
-    messages.style.color = 'yellow';
-    messages.innerHTML = errorMsg;
-    messages.style.textAlign = 'center';
+function createListItem(item) {
+	let li = document.createElement("li");
+	li.appendChild(document.createTextNode(item));
+	return li;
+}
 
-    setTimeout( () =>{
-        messages.innerHTML = '';
-        messages.style.background = 'white';
-    }, 3000);
+function showError(errorMsg, type) {
+	const messages = document.getElementById("messages");
+	messages.style.background = "red";
+	messages.style.color = "yellow";
+	messages.innerHTML = errorMsg;
+	messages.style.textAlign = "center";
+
+	setTimeout(() => {
+		messages.innerHTML = "";
+		messages.style.background = "white";
+	}, 3000);
 }
