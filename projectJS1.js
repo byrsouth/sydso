75aeb4fe5a09be252d379e9e7da19d7ba0d387f5
Bysouth <byron.southerland@icloud.com>
2022-07-18 11:51:02 -0400

    fixed formatting

--- projectJS1.js
+++ projectJS1.js
@@ -1,234 +1,224 @@
-document.getElementById('createResume').addEventListener('click', buildResume2);
+document.getElementById("createResume").addEventListener("click", buildResume2);
 const codingSkills = [];
 const employmentDetails = [];
-const skillForm = document.getElementById('codingSkills');
-const addNewSkillBtn = document.getElementById('addNewSkillBtn');
-const addEmploymentBtn = document.getElementById('addEmploymentBtn');
+const educationDetail = null;
+const skillForm = document.getElementById("codingSkills");
 
-addNewSkillBtn.addEventListener('click', () => {
-	const input = document.getElementById(`skill_${codingSkills.length}`);
-	if (input.value === '') {
-		alert('Must fill in skill');
-	} else {
-		const skill = document.getElementById(
-			`skill_${codingSkills.length}`
-		).value;
-		codingSkills.push(skill);
-		createSkillInput();
-		createEducationInput();
-		console.log(codingSkills);
-	}
+const addNewSkillBtn = document.getElementById("addNewSkillBtn");
+const addEmploymentBtn = document.getElementById("addEmploymentBtn");
+const addEducationBtn = document.getElementById("addEduBtn");
+
+addNewSkillBtn.addEventListener("click", () => {
+  const input = document.getElementById(`skill_${codingSkills.length}`);
+  if (input.value === "") {
+    const skill = document.getElementById(`skill_${codingSkills.length}`).value;
+    codingSkills.push(skill);
+    createSkillInput();
+    console.log(codingSkills);
+  } else {
+    alert("Must fill in skill");
+  }
 });
 
-addEmploymentBtn.addEventListener('click', () => {
-	createEmploymentInput();
+addEmploymentBtn.addEventListener("click", () => {
+  employmentDetail = document.getElementById(`empDetail_${employmentDetails.length}`);
+  
+  if (employmentDetail.value) {
+    employmentDetails.push(employmentDetail);
+    createEmploymentInput();
+  } else {
+    alert("Must fill in employment history");
+  }
 });
 
-function buildResume2() {
-	flyWindow = window.open(
-		'about:blank',
-		'myPop',
-		'width=1600,height=1000,left=400,top=400'
-	);
-	buildStartHTML(flyWindow);
-	buildNameSection(flyWindow);
-	buildContactSection(flyWindow);
-	
-	buildHTMLEnd(flyWindow);
-}
-
-function buildStartHTML(flyWindow) {
-	flyWindow.document.write(
-		"<html>\n<head>\n<title>Web 115 Final Project</title>\n<link rel='stylesheet' href='project.css'>\n</head>\n<body>\n"
-	);
-}
-
-function buildHTMLEnd() {
-	flyWindow.document.write('</div>\n</body>\n</html>');
-}
-
-function buildNameSection(flyWindow) {
-	userName = document.getElementById('fullName').value;
-	flyWindow.document.write(`<div id='guestName'>${userName}</div>`);
-}
-
-function buildContactSection(flyWindow) {
-	userCity = document.getElementById('city').value;
-	userState = document.getElementById('state').value;
-	userZip = document.getElementById('zipCode').value;
-	userPhone = document.getElementById('phoneNumber').value;
-	userEmail = document.getElementById('email').value;
-
-	userSocialMedia = document.getElementById('socialMedia').value;
-	userPortfolio = document.getElementById('portfolio').value;
-
-	const myText = `<div id='contact'>${userCity},${userState} ${userZip} &#9830; ${userPhone} &#9830; ${userEmail}
-	                ${userSocialMedia} &#9830; ${userPortfolio}</div>`;
-	flyWindow.document.write(myText);
-}
-
-function buildSkillsSection(flyWindow) {
-	const myText = "<div id='skills'>";
-	myText += '<h1>Skills</h1>';
-	myText += '<h3>Coding Skills</h3>';
-	flyWindow.document.write(myText);
-}
-
-function buildResume() {
-	// userCdSkills = document.getElementById("codingSkills").value;
-	// userWrkEthic = document.getElementById("workEthic").value;
-	// userWritingSkills = document.getElementById("writingSkills").values;
-	// userTechSkills = document.getElementById("techSkills").value;
-	// userTechSkills2 = document.getElementById("techSkills2").value;
-	// userEDU = document.getElementById("eduBkgrd").value;
-	// userStartDate1 = document.getElementById("startDate1").value;
-	// userEmpDetails1 = document.getElementById("empDetails1").value;
-	// userEndDate1 = document.getElementById("endDate1").value;
-	// userStartDate2 = document.getElementById("startDate2").value;
-	// userEndDate2 = document.getElementById("endDate2").value;
-	// userEmpDetails2 = document.getElementById("empDetails2").value;
-	// userStartDate3 = document.getElementById("startDate3").value;
-	// userEndDate3 = document.getElementById("endDate3").value;
-	// userEmpDetails3 = document.getElementById("empDetails3").value;
-	// userRef = document.getElementById("busRef").value;
-
-	myText =
-		"<html>\n<head>\n<title>Welcome</title>\n<link rel='stylesheet' href='project.css'>\n</head>\n<body>\n<div id='guestName'>";
-
-	myText += userCdSkills + ' ';
-	myText += '<h3>Work Ethic</h3>';
-	myText += userWrkEthic + ' ';
-	myText += '<h3>Writing Skills</h3>';
-	myText += userWritingSkills;
-	myText += '</div>';
-
-	myText += "<div id='techSkills'>";
-	myText += '<h1>Technical Skills</h1>';
-	myText += userTechSkills + ' ';
-	myText += userTechSkills2 + ' ';
-	myText += '</div>';
-
-	myText += "<div id='edu'>";
-	myText += '<h1>Educational Background</h1>';
-	myText += userEDU + ' ';
-	myText += '</div>';
-
-	myText += "<div id='empHistory1'>";
-	myText += '<h1>Work History</h1>';
-	myText += userStartDate1 + '  ' + userEndDate1 + ', ';
-	myText += userEmpDetails1;
-	myText += '</div>';
-
-	myText += "<div id='empHistory2'>";
-	myText += userStartDate2 + '  ' + userEndDate2 + ', ';
-	myText += userEmpDetails2;
-	myText += '</div>';
-
-	myText += "<div id='empHistory3'>";
-	myText += userStartDate3 + ' ' + userEndDate3;
-	myText += userEmpDetails3;
-	myText += '</div>';
-
-	myText += "<div id='bzRef'>";
-	myText += '<h1>Business References</h1>';
-	myText += userRef;
-	myText += '</div>\n</body>\n</html>';
-
-	flyWindow = window.open(
-		'about:blank',
-		'myPop',
-		'width=800,height=400,left=400,top=400'
-	);
-	flyWindow.document.write(myText);
-}
-
-function createEducationInput() {
-	const span = document.createElement('span');
-	const startDatePicker = document.createElement('input');
-	const endDatePicker = document.createElement('input');
-	const eduDetails = document.createElement('textarea');
-	const div = document.createElement('div');
-
-	const startDateLabel = document.createElement('label');
-	const startDateLabelText = document.createTextNode('Start Date ');
-	startDateLabel.appendChild(startDateLabelText);
-
-	const endDateLabel = document.createElement('label');
-	const endDateLabelText = document.createTextNode('End Date ');
-	endDateLabel.appendChild(endDateLabelText);
-
-	endDatePicker.type = 'date';
-	endDatePicker.id = 'endDate';
-
-	startDatePicker.type = 'date';
-	startDatePicker.id = 'startDate';
-
-	span.appendChild(startDateLabel);
-	span.appendChild(startDatePicker);
-
-	span.appendChild(endDateLabel);
-	span.appendChild(endDatePicker);
-
-	eduDetails.id = 'eduDetails';
-
-	eduDetails.class = 'empDetails';
-	eduDetails.rows = 10;
-	eduDetails.cols = 60;
-	eduDetails.placeholder = 'Please describe your education.';
-
-	div.appendChild(eduDetails);
-
-	document.getElementById('eduHistory').appendChild(span);
-	document.getElementById('eduHistory').appendChild(div);
-}
+addEducationBtn.addEventListener("click", () => {
+  educationDetail = document.getElementById("educationDetail");
+});
 
 function createSkillInput() {
-	const span = document.createElement('span');
-	const input = document.createElement('input');
-	input.id = `skill_${codingSkills.length}`;
-	input.placeholder = 'Enter skill';
-	span.appendChild(input);
-	skillForm.appendChild(span);
+  const span = document.createElement("span");
+  const input = document.createElement("input");
+  input.id = `skill_${codingSkills.length}`;
+  input.placeholder = "Enter skill";
+  span.appendChild(input);
+  skillForm.appendChild(span);
 }
 
 function createEmploymentInput() {
-	const span = document.createElement('span');
-	const startDatePicker = document.createElement('input');
-	const endDatePicker = document.createElement('input');
-	const empDetails = document.createElement('textarea');
-	const div = document.createElement('div');
+  const empDetailId = `empDetail_${employmentDetails.length}`;
+  const employmentHistoryComponent = historyComponent(
+    empDetailId,
+    "Please describe your work experiences."
+  );
+  document.getElementById("empHistory").appendChild(employmentHistoryComponent);
+}
 
-	const startDateLabel = document.createElement('label');
-	const startDateLabelText = document.createTextNode('Start Date ');
-	startDateLabel.appendChild(startDateLabelText);
+function createEducationInput() {
+  const eduDetailId = "educationDetail";
+  const educationHistoryComponent = historyComponent(
+    eduDetailId,
+    "Please describe your education."
+  );
+  document.getElementById("eduHistory").appendChild(educationHistoryComponent);
+}
 
-	const endDateLabel = document.createElement('label');
-	const endDateLabelText = document.createTextNode('End Date ');
-	endDateLabel.appendChild(endDateLabelText);
+function historyComponent(id, placeholder) {
+  const span = document.createElement("span");
+  const startDatePicker = document.createElement("input");
+  const endDatePicker = document.createElement("input");
+  const historyTextArea = document.createElement("textarea");
+  const div = document.createElement("div");
 
-	endDatePicker.type = 'date';
-	endDatePicker.id = 'endDate';
+  const startDateLabel = document.createElement("label");
+  const startDateLabelText = document.createTextNode("Start Date ");
+  startDateLabel.appendChild(startDateLabelText);
 
-	startDatePicker.type = 'date';
-	startDatePicker.id = 'startDate';
+  const endDateLabel = document.createElement("label");
+  const endDateLabelText = document.createTextNode("End Date ");
+  endDateLabel.appendChild(endDateLabelText);
 
-	span.appendChild(startDateLabel);
-	span.appendChild(startDatePicker);
+  endDatePicker.type = "date";
+  endDatePicker.id = "endDate";
 
-	span.appendChild(endDateLabel);
-	span.appendChild(endDatePicker);
+  startDatePicker.type = "date";
+  startDatePicker.id = "startDate";
 
-	empDetails.id = `empDetail_${employmentDetails.length}`;
-	empDetails.class = 'empDetials';
-	empDetails.rows = 10;
-	empDetails.cols = 60;
-	empDetails.placeholder = 'Please describe your work experiences.';
+  span.appendChild(startDateLabel);
+  span.appendChild(startDatePicker);
 
-	div.appendChild(empDetails);
+  span.appendChild(endDateLabel);
+  span.appendChild(endDatePicker);
 
-	document.getElementById('empHistory').appendChild(span);
-	document.getElementById('empHistory').appendChild(div);
+  historyTextArea.id = id;
+  historyTextArea.class = "empDetials";
+  historyTextArea.rows = 10;
+  historyTextArea.cols = 60;
+  historyTextArea.placeholder = placeholder;
+
+  div.appendChild(span);
+  div.appendChild(historyTextArea);
+
+  return div;
+}
+
+function buildResume() {
+  // userCdSkills = document.getElementById("codingSkills").value;
+  // userWrkEthic = document.getElementById("workEthic").value;
+  // userWritingSkills = document.getElementById("writingSkills").values;
+  // userTechSkills = document.getElementById("techSkills").value;
+  // userTechSkills2 = document.getElementById("techSkills2").value;
+  // userEDU = document.getElementById("eduBkgrd").value;
+  // userStartDate1 = document.getElementById("startDate1").value;
+  // userEmpDetails1 = document.getElementById("empDetails1").value;
+  // userEndDate1 = document.getElementById("endDate1").value;
+  // userStartDate2 = document.getElementById("startDate2").value;
+  // userEndDate2 = document.getElementById("endDate2").value;
+  // userEmpDetails2 = document.getElementById("empDetails2").value;
+  // userStartDate3 = document.getElementById("startDate3").value;
+  // userEndDate3 = document.getElementById("endDate3").value;
+  // userEmpDetails3 = document.getElementById("empDetails3").value;
+  // userRef = document.getElementById("busRef").value;
+
+  myText =
+    "<html>\n<head>\n<title>Welcome</title>\n<link rel='stylesheet' href='project.css'>\n</head>\n<body>\n<div id='guestName'>";
+
+  myText += userCdSkills + " ";
+  myText += "<h3>Work Ethic</h3>";
+  myText += userWrkEthic + " ";
+  myText += "<h3>Writing Skills</h3>";
+  myText += userWritingSkills;
+  myText += "</div>";
+
+  myText += "<div id='techSkills'>";
+  myText += "<h1>Technical Skills</h1>";
+  myText += userTechSkills + " ";
+  myText += userTechSkills2 + " ";
+  myText += "</div>";
+
+  myText += "<div id='edu'>";
+  myText += "<h1>Educational Background</h1>";
+  myText += userEDU + " ";
+  myText += "</div>";
+
+  myText += "<div id='empHistory1'>";
+  myText += "<h1>Work History</h1>";
+  myText += userStartDate1 + "  " + userEndDate1 + ", ";
+  myText += userEmpDetails1;
+  myText += "</div>";
+
+  myText += "<div id='empHistory2'>";
+  myText += userStartDate2 + "  " + userEndDate2 + ", ";
+  myText += userEmpDetails2;
+  myText += "</div>";
+
+  myText += "<div id='empHistory3'>";
+  myText += userStartDate3 + " " + userEndDate3;
+  myText += userEmpDetails3;
+  myText += "</div>";
+
+  myText += "<div id='bzRef'>";
+  myText += "<h1>Business References</h1>";
+  myText += userRef;
+  myText += "</div>\n</body>\n</html>";
+
+  flyWindow = window.open(
+    "about:blank",
+    "myPop",
+    "width=800,height=400,left=400,top=400"
+  );
+  flyWindow.document.write(myText);
+}
+
+function buildResume2() {
+  flyWindow = window.open(
+    "about:blank",
+    "myPop",
+    "width=1600,height=1000,left=400,top=400"
+  );
+  buildStartHTML(flyWindow);
+  buildNameSection(flyWindow);
+  buildContactSection(flyWindow);
+
+  buildHTMLEnd(flyWindow);
+}
+
+function buildStartHTML(flyWindow) {
+  flyWindow.document.write(
+    "<html>\n<head>\n<title>Web 115 Final Project</title>\n<link rel='stylesheet' href='project.css'>\n</head>\n<body>\n"
+  );
+}
+
+function buildHTMLEnd() {
+  flyWindow.document.write("</div>\n</body>\n</html>");
+}
+
+function buildNameSection(flyWindow) {
+  userName = document.getElementById("fullName").value;
+  flyWindow.document.write(`<div id='guestName'>${userName}</div>`);
+}
+
+function buildContactSection(flyWindow) {
+  userCity = document.getElementById("city").value;
+  userState = document.getElementById("state").value;
+  userZip = document.getElementById("zipCode").value;
+  userPhone = document.getElementById("phoneNumber").value;
+  userEmail = document.getElementById("email").value;
+
+  userSocialMedia = document.getElementById("socialMedia").value;
+  userPortfolio = document.getElementById("portfolio").value;
+
+  const myText = `<div id='contact'>${userCity},${userState} ${userZip} &#9830; ${userPhone} &#9830; ${userEmail}
+                      ${userSocialMedia} &#9830; ${userPortfolio}</div>`;
+  flyWindow.document.write(myText);
+}
+
+function buildSkillsSection(flyWindow) {
+  const myText = "<div id='skills'>";
+  myText += "<h1>Skills</h1>";
+  myText += "<h3>Coding Skills</h3>";
+  flyWindow.document.write(myText);
 }
 
 createSkillInput();
 createEmploymentInput();
+createEducationInput();
