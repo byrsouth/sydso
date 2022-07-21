590ba5de187c3be6c328287c8b4ae81f066ef7ae
Bysouth <byron.southerland@icloud.com>
2022-07-21 15:20:06 -0400

    Sklls component

--- resume/src/components/skilll.component.js
+++ resume/src/components/skilll.component.js
@@ -1,15 +1,26 @@
-export class SkillComponent{
+export class SkillComponent {
+  skillsspan = document.createElement("span");
 
-	skillsspan = document.createElement('span');
+  constructor(id, placeholder) {
+    const input = document.createElement("input");
+    input.id = id;
+    input.placeholder = placeholder;
+    this.skillsspan.appendChild(input);
+  }
 
-	constructor(id, placeholder){
-		const input = document.createElement('input');
-		input.id = id;
-		input.placeholder = placeholder;
-		this.skillsspan.appendChild(input);
-	}
+  renderComponent() {
+    return this.skillsspan;
+  }
 
-	renderComponent(){
-		return this.skillsspan;	
-	}
+  static renderRenderReadOnly(e) {
+    console.log("Parent = " + e.parentElement.id);
+    let parent = e.parentElement;
+
+    const element = document.createElement("span");
+    const text = document.createTextNode(e.value);
+    element.classList.add("readOnlyLink");  
+
+    parent.replaceChild(element, e);
+    //return element;
+  }
 }
